2b9fba1480260875b07360a2543ee78c53d21278
Christopher Fujino <christopherfujino@gmail.com>
2016-06-28 03:28:34 -1000

    added code to allow multiple personas

--- bot.js
+++ bot.js
@@ -1,17 +1,17 @@
 let Botkit = require('botkit'),
     fs = require('fs'),
-    token = JSON.parse(fs.readFileSync('token.json', 'utf8')).token,
     personas = JSON.parse(fs.readFileSync('personas.json', 'utf8')),
-    controller = Botkit.slackbot(),
-    bot = controller.spawn({'token' : token});
+    controller = Botkit.slackbot();
 
-  const persona = personas['ringo'];  // change to access different persona
+  const persona = personas['brendanbot'];  // change to access different persona
+  const token = JSON.parse(fs.readFileSync('tokens.json', 'utf8'))[persona.name]['token'];
+  let bot = controller.spawn({'token' : token});
 
   bot.startRTM(function(err){
     if(err) throw err;
   });
 
-  controller.hears([persona.name], ['direct_message','direct_mention','mention','ambient'], function(bot, message){
+  controller.hears([persona['name']], ['direct_message','direct_mention','mention'], function(bot, message){
     let x = Math.floor(Math.random()*persona.quotes.length);
     bot.reply(message, persona.quotes[x]);
   });
